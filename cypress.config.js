113a6ff8e72c6e97d56f7490a77dece4f203b868
jeffsbarbosa <jbviolao@gmail.com>
2024-12-19 18:49:19 -0300

    correção de links

--- cypress.config.js
+++ cypress.config.js
@@ -14,14 +14,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     pageLoadTimeout: 60000,
-    //baseUrl: 'https://graduacao4.ung.digital',
-    //specPattern: 'cypress/e2e/*/.{js,jsx,ts,tsx}',
-     //https://graduacao5.uninorte.digital
-     //https://graduacao4.uninassau.digital
-     //https://graduacao4.unama.digital
-     //https://graduacao4.uninorte.digital
-     //https://graduacao4.unifacimed.digital
-     //https://graduacao4.uninabuco.digital
-     //https://graduacao4.ung.digital
+ 
   },
 });
