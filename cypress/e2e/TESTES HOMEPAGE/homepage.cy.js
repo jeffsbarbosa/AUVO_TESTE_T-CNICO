d63422bf6d5e5e646db2b77c8efc97260b337a2f
jeffsbarbosa <jbviolao@gmail.com>
2024-12-19 18:40:24 -0300

    alteração do describe

--- cypress/e2e/TESTES HOMEPAGE/homepage.cy.js
+++ cypress/e2e/TESTES HOMEPAGE/homepage.cy.js
@@ -2,7 +2,7 @@
 
 import util from "../../support/util"
 
-describe('Disponibilidade da homepage', () => {
+describe('Disponibilidade da homepage saucedemo', () => {
    
     it('CT01 - Validando se estou na homepage e se está acessível', () => {
         util.acessarHomepage()
